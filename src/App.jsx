0b7f5bd305cf8ec80b25a6726b7de7b245ea9dcd
himelmia535 <himelmia535@gmail.com>
2024-03-24 21:52:02 +0600

    navbar complete with theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
-
 import './App.css'
+import Home from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button className="btn btn-secondary">Secondary</button>
+      <Home></Home>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import Nav from "../components/Nav";
+
+const Home = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            <h1>Home Page</h1>
+        </div>
+    );
+};
+
+export default Home;
